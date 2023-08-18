d1ee059330f080ccd8ea8560fe8a17df5db91278
Nazim Budaqlı <89692424+NazimBudaqov@users.noreply.github.com>
2023-08-18 12:16:19 +0400

    message

--- app/apis/urls.py
+++ app/apis/urls.py
@@ -18,6 +18,18 @@ urlpatterns = [
     path('rank-difference/', RankDifferenceApiView.as_view(), name='rank_difference'),
     path('line-chart/', LineChartApiView.as_view(), name='line_chart'),
     path('bump-chart/', BumpChartApiView.as_view(), name='bump_chart'),
+
+    # path('', SectSubsectIndicaView.as_view(), name='get-default-data'),
+    # path('countries-data/<str:indicator_name>/', CountriesView.as_view(), name='get-related-countries'),
+    # path('available-years/', AvailableYearsView.as_view(), name='get-available-years'),
+    # path('ranks/', AvailableRanksView.as_view(), name='get-available-ranks'),
+    
+    # #?year1=2010&year2=2020&ranks=5,172
+    # path('diagramsData/', AllDiagramsView.as_view(),name='years-data'),
+    # path('by_amount/', AmountView.as_view(), name='diagram1-only'),
+    # path('rank_diff/', RankDifferenceView.as_view(),name='diagram2-only'),
+    # path('years_data/', RankAmountDiagrams.as_view(),name='diagram3&4-only'),
+
     #second dashboard
     path('country-info/', CountryInfoApiView.as_view(), name='country_info'),
     path('country-diagram/', CountryIndicaDiagramApiView.as_view(), name='country_diagram'),
---
+++ app/apis/views/default_views/__init__.py
@@ -0,0 +1,4 @@
+from .get_years import AvailableYearsView
+from .get_ranks import AvailableRanksView
+from .get_countries import CountriesView
+from .get_default import SectSubsectIndicaView
